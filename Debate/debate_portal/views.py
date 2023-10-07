69e8be317d81c0867b466f5571bc21aabe587b3d
Ayush Singh <80458699+Ayushs475@users.noreply.github.com>
2023-10-07 13:04:18 +0530

    views.py

--- Debate/debate_portal/views.py
+++ Debate/debate_portal/views.py
@@ -1,11 +1,20 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import CustomUser,debate,vote
-from .serializers import CustomUserSerializer,DebateSerializer,VoteSerializer
+from rest_framework.authentication import TokenAuthentication  # Import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated  # Import IsAuthenticated
+from .models import CustomUser, Debate, Vote
+from .serializers import CustomUserSerializer, DebateSerializer, VoteSerializer
 from django.shortcuts import get_object_or_404
+import logging  # Import the logging module
+
+# Create a logger instance
+logger = logging.getLogger(__name__)
 
 class UserRegistrationView(APIView):
+    authentication_classes = [TokenAuthentication]  # Apply Token Authentication
+    permission_classes = [IsAuthenticated]  # Apply IsAuthenticated permission
+
     def get(self, request, username):
         # Retrieve user by username or return 404 if not found
         user = get_object_or_404(CustomUser, username=username)
@@ -34,7 +43,13 @@ class UserRegistrationView(APIView):
         user.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+    def perform_create(self, serializer):
+        serializer.save()
+
 class DebateManagementView(APIView):
+    authentication_classes = [TokenAuthentication]  # Apply Token Authentication
+    permission_classes = [IsAuthenticated]  # Apply IsAuthenticated permission
+
     def get(self, request):
         debates = Debate.objects.all()
         serializer = DebateSerializer(debates, many=True)
@@ -48,13 +63,18 @@ class DebateManagementView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class DebateDetailView(APIView):
+    authentication_classes = [TokenAuthentication]  # Apply Token Authentication
+    permission_classes = [IsAuthenticated]  # Apply IsAuthenticated permission
+
     def get(self, request, debate_id):
-        debate = Debate.objects.get(pk=debate_id)
+        debate = get_object_or_404(Debate, pk=debate_id)
         serializer = DebateSerializer(debate)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-
 class VoteView(APIView):
+    authentication_classes = [TokenAuthentication]  # Apply Token Authentication
+    permission_classes = [IsAuthenticated]  # Apply IsAuthenticated permission
+
     def post(self, request):
         serializer = VoteSerializer(data=request.data)
         if serializer.is_valid():
@@ -63,6 +83,9 @@ class VoteView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class BanUserView(APIView):
+    authentication_classes = [TokenAuthentication]  # Apply Token Authentication
+    permission_classes = [IsAuthenticated]  # Apply IsAuthenticated permission
+
     def post(self, request, username):
         user = CustomUser.objects.get(username=username)
         # Implement logic to ban the user (e.g., set a 'banned' flag)
@@ -71,6 +94,9 @@ class BanUserView(APIView):
         return Response({'message': f'{username} has been banned'}, status=status.HTTP_200_OK)
 
 class AssignModeratorRoleView(APIView):
+    authentication_classes = [TokenAuthentication]  # Apply Token Authentication
+    permission_classes = [IsAuthenticated]  # Apply IsAuthenticated permission
+
     def post(self, request, username):
         user = CustomUser.objects.get(username=username)
         # Implement logic to assign the moderator role
@@ -79,8 +105,10 @@ class AssignModeratorRoleView(APIView):
         return Response({'message': f'{username} has been assigned the moderator role'}, status=status.HTTP_200_OK)
 
 class ReportUserInfractionView(APIView):
+    authentication_classes = [TokenAuthentication]  # Apply Token Authentication
+    permission_classes = [IsAuthenticated]  # Apply IsAuthenticated permission
+
     def post(self, request, username):
         # Implement logic to report user infractions
         # This could involve recording and tracking reports
         return Response({'message': f'User {username} has been reported for an infraction'}, status=status.HTTP_200_OK)
-
