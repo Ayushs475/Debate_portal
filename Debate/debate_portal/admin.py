6492ac16f05878c968afe5065ad8552ccb34b847
Ayush Singh <80458699+Ayushs475@users.noreply.github.com>
2023-10-07 12:59:41 +0530

    Update admin.py

--- Debate/debate_portal/admin.py
+++ Debate/debate_portal/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import CustomUser, Debate, Vote
 
-# Register your models here.
+# Register the CustomUser model
+admin.site.register(CustomUser)
+
+# Register the Debate model
+admin.site.register(Debate)
+
+# Register the Vote model
+admin.site.register(Vote)
